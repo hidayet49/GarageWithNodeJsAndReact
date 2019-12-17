ee684c076a1497c7dcd4e584ad311124676106fc
Hidayet Gencer <hidayet.gencer@hs-augsburg.de>
2019-12-17 15:57:01 +0100

    addin additional test conditions for email and password

--- test/all/login.js
+++ test/all/login.js
@@ -28,4 +28,54 @@ describe('ALL /login',()=>{
         })
         .catch(error=>done(error))
     })
+    it('Fail, wrong password test',(done)=>{
+        request(app).post('/api/all/login')
+        .send({
+            email:"test@gmail.com",
+            password:"abc1234"
+        })
+        .then((res)=>{
+            expect(res.text).equal('Invalid Password');
+            done();
+        })
+        .catch(error=>done(error))
+    })
+    it('Fail, wrong email test',(done)=>{
+        request(app).post('/api/all/login')
+        .send({
+            email:"noTest@gmail.com",
+            password:"abc1234"
+        })
+        .then((res)=>{
+            expect(res.text).equal('Your email  is wrong');
+            done();
+        })
+        .catch(error=>done(error))
+    })
+    it('Fail, valid email address check!',(done)=>{
+        request(app).post('/api/all/login')
+        .send({
+            email:"test@gmail",
+            password:"abc12345",
+        })
+        .then((res)=>{         
+            expect(res.status).equal(400);    
+            expect(res.text).equal(`"email" must be a valid email`);
+            done();
+        })
+        .catch(error=>done(error))
+    })
+    it('Fail, password character validation',(done)=>{
+        request(app).post('/api/all/login')
+        .send({
+            email:"test@gmail.com",
+            password:"abc1",
+        })
+        .then((res)=>{         
+            expect(res.status).equal(400);    
+            expect(res.text).equal(`"password" length must be at least 6 characters long`);
+            done();
+        })
+        .catch(error=>done(error))
+    })
 })
