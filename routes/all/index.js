ffe93d12f45bc15920dfa318af88abdf5527fd74
Hidayet Gencer <hidayet.gencer@hs-augsburg.de>
2019-12-17 14:45:28 +0100

    delete whitspaces

--- routes/all/index.js
+++ routes/all/index.js
@@ -1,11 +1,10 @@
-const allRouter = require('express').Router();
-const register = require('./register');
-const login=require('./login');
+const allRouter = require("express").Router();
+const register = require("./register");
+const login = require("./login");
 
 //REGISTRATION routing
-allRouter.post('/register',register);
+allRouter.post("/register", register);
 //LOGIN routing
-allRouter.post('/login',login);
-
+allRouter.post("/login", login);
 
 module.exports = allRouter;
